5b39c6eddb5bd057618646c5dc8a71b8d081be66
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-02-19 15:33:46 +0100

    milestone 2 exercise completed

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.base')
+
+
+@section('content')
+    <div class="jumbo-container">
+        <span class="default-badge-bg">CURRENT SERIES</span>
+    </div>
+    <div>
+        <div class="container py-5">
+            <div class="col-12 py-5">
+                <h1>Qua non ci sta nulla ancora...</h1>
+            </div>
+        </div>
+    </div>
+@endsection
